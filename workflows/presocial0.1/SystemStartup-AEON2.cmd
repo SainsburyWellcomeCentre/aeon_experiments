0e4979262af20651a04322fb4e18611d722f3834
glopesdev <glopesdev@users.noreply.github.com>
2023-06-05 10:43:55 +0100

    Make synchronization optional in startup script

--- workflows/presocial0.1/SystemStartup-AEON2.cmd
+++ workflows/presocial0.1/SystemStartup-AEON2.cmd
@@ -1,2 +1,1 @@
-cd "C:\ProjectAeon\aeon_experiments\workflows\presocial0.1"
-"..\..\bonsai\Bonsai.exe" SystemStartup-AEON2.bonsai --no-editor
+"..\..\bonsai\Bonsai.exe" SystemStartup-AEON2.bonsai --no-editor %*
