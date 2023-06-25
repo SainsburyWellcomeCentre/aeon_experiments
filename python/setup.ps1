f3a883b733d1b44ef436648504a6f084648b579d
glopesdev <glopesdev@users.noreply.github.com>
2023-06-25 10:12:43 +0100

    Bootstrap python using virtual environment

--- python/setup.ps1
+++ python/setup.ps1
@@ -1,9 +1,4 @@
-if (!(Test-Path "./winpython")) {
-    Invoke-WebRequest "https://github.com/winpython/winpython/releases/download/4.3.20210620/Winpython64-3.9.5.0dot.exe" -OutFile "temp.exe"
-	$process = Start-Process "temp.exe" "-y" -PassThru
-	$process.WaitForExit()
-	Rename-Item -Path "WPy64-3950" -NewName "winpython"
-    Remove-Item -Path "temp.exe"
+if (!(Test-Path "./venv")) {
+	python.exe -m venv .venv
+	.\.venv\Scripts\pip.exe install -r .\requirements.txt
 }
-
-cmd.exe /c '.\winpython\scripts\env.bat && pip install -r requirements.txt'
