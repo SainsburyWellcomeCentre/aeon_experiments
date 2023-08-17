a9698738ad8d04828fe6d62adb027d2f85b6f560
Jai Bhagat <jkbhagatio@gmail.com>
2023-08-17 22:57:44 +0100

    Updated config after fresh install

--- bonsai/setup.ps1
+++ bonsai/setup.ps1
@@ -1,12 +1,19 @@
 if (!(Test-Path "./Bonsai.exe")) {
-    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.7.2/Bonsai.zip" -OutFile "temp.zip"
+    $release = "https://github.com/bonsai-rx/bonsai/releases/latest/download/Bonsai.zip"
+    $configPath = "./Bonsai.config"
+    if (Test-Path $configPath) {
+        [xml]$config = Get-Content $configPath
+        $bootstrapper = $config.PackageConfiguration.Packages.Package.where{$_.id -eq 'Bonsai'}
+        if ($bootstrapper) {
+            $version = $bootstrapper.version
+            $release = "https://github.com/bonsai-rx/bonsai/releases/download/$version/Bonsai.zip"
+        }
+    }
+    Invoke-WebRequest $release -OutFile "temp.zip"
     Move-Item -Path "NuGet.config" "temp.config"
     Expand-Archive "temp.zip" -DestinationPath "." -Force
     Move-Item -Path "temp.config" "NuGet.config" -Force
     Remove-Item -Path "temp.zip"
-    Remove-Item -Path "Bonsai32.exe"
+	Remove-Item -Path "Bonsai32.exe"
 }
 & .\Bonsai.exe --no-editor
-Get-ChildItem -Path "Packages" -Recurse -Filter *git2-*.dll |
-    Where-Object FullName -NotLike "*win-x86*" |
-    Copy-Item -Destination "." -Force
