afcc0f75538a0acfaf5f37925b7ab6e379b23777
glopesdev <glopesdev@users.noreply.github.com>
2024-04-06 12:57:31 +0100

    Add numeric type conversions for logging

--- workflows/ephys/Extensions/ConvertToBytes.cs
+++ workflows/ephys/Extensions/ConvertToBytes.cs
@@ -1,15 +1,25 @@
 using Bonsai;
 using System;
 using System.ComponentModel;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Numerics;
 
 [Combinator]
 [Description("Convert various data representations to a serializable byte array.")]
 [WorkflowElementCategory(ElementCategory.Transform)]
 public class ConvertToBytes
 {
+    public IObservable<float[]> Process(IObservable<Quaternion> source)
+    {
+        return source.Select(value => new[] { value.W, value.X, value.Y, value.Z });
+    }
+
+    public IObservable<float[]> Process(IObservable<Vector3> source)
+    {
+        return source.Select(value => new[] { value.X, value.Y, value.Z });
+    }
+
     public IObservable<byte[]> Process(IObservable<ulong> source)
     {
         return source.Select(value => BitConverter.GetBytes(value));
