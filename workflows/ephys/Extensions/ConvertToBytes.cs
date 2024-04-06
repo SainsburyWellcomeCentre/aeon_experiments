9f3a76ca4a2184f05ce8872d787da357764eea69
glopesdev <glopesdev@users.noreply.github.com>
2024-04-06 12:29:49 +0100

    Add imu data logging

---
+++ workflows/ephys/Extensions/ConvertToBytes.cs
@@ -0,0 +1,22 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("Convert various data representations to a serializable byte array.")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class ConvertToBytes
+{
+    public IObservable<byte[]> Process(IObservable<ulong> source)
+    {
+        return source.Select(value => BitConverter.GetBytes(value));
+    }
+
+    public IObservable<byte[]> Process(IObservable<int> source)
+    {
+        return source.Select(value => BitConverter.GetBytes(value));
+    }
+}
