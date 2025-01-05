0e7302f5940628969ec11c77610421ef0222cf5c
aspaNeuro <a.almeida@neurogears.org>
2025-01-05 14:49:43 +0000

    Add Metadata  from social experiments

--- calibration/metadata.py
+++ calibration/metadata.py
@@ -8,13 +8,13 @@ import sys
 parser = argparse.ArgumentParser(description="Exports device and experiment metadata for the specified workflow file.")
 parser.add_argument('workflow', type=str, help="The path to the workflow file used for data acquisition.")
 parser.add_argument('--indent', type=int, help="The optional indent level for JSON pretty printing.")
-parser.add_argument('--allow-dirty', action="store_true", help="Optionally allow exporting metadata for modified repositories.")
+parser.add_argument('--strict', action="store_true", help="Optionally enforce exporting metadata only for clean repositories.")
 parser.add_argument('--output', type=str, help="The optional path to the exported JSON file.")
 args = parser.parse_args()
 dname = Path(__file__).parent
 
 repo = Repo(dname.parent)
-if not args.allow_dirty and (repo.is_dirty() or len(repo.untracked_files) > 0):
+if args.strict and (repo.is_dirty() or len(repo.untracked_files) > 0):
     parser.error("all modifications to the acquisition repository must be committed before exporting metadata")
 
 ns = {
@@ -39,11 +39,13 @@ def list_metadata(elements, **kwargs):
             nodes = x.xpath('./x:Workflow/x:Nodes', namespaces=ns)[0]
             metadata[name] = list_metadata(nodes)
         elif grouptype != "ExternalizedMapping":
-            elem_metadata = {}
-            elem_metadata = (recursive_dict(x)[1])
+            elem_metadata = recursive_dict(x)[1]
             if elem_metadata is None:
                 continue
             elem_metadata.update(kwargs)
+            if grouptype == "IncludeWorkflow":
+                path = Path(x.get('Path')).stem
+                elem_metadata['Type'] = path.rsplit(':', 1)[-1]
             metadata |= elem_metadata
     return metadata
 
@@ -57,7 +59,9 @@ metadata = {
 metadata |= list_metadata(metadata_group)
 
 if args.output:
-    with open(args.output, "w") as outfile:
+    outpath = Path(args.output)
+    outpath.parent.mkdir(parents=True, exist_ok=True)
+    with open(outpath, "w") as outfile:
         json.dump(metadata, outfile, indent=args.indent)
 else:
     json.dump(metadata, sys.stdout, indent=args.indent)
