b79fb50f66de1c0e1102606cf2c198002bf8b36e
aspaNeuro <a.almeida@neurogears.org>
2025-03-05 12:12:18 +0000

    Merge remote-tracking branch 'origin/social' into social-ephys

--- workflows/social/Extensions/ValidateEnvironment.cs
+++ workflows/social/Extensions/ValidateEnvironment.cs
@@ -8,10 +8,17 @@ using System.Reactive.Linq;
 [WorkflowElementCategory(ElementCategory.Sink)]
 public class ValidateEnvironment
 {
+    public string Path { get; set; }
+
     public IObservable<Social.Environment> Process(IObservable<Social.Environment> source)
     {
         return source.Do(environment =>
         {
+            if (string.IsNullOrEmpty(environment.Name))
+            {
+                environment.Name = System.IO.Path.GetFileNameWithoutExtension(Path);
+            }
+
             foreach (var block in environment.Blocks)
             {
                 foreach (var patch in block.Patches.Values)
