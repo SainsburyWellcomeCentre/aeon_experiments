cb9108be31f90a8a1b703f7d826c5346cba650c2
aspaNeuro <a.almeida@neurogears.org>
2025-06-04 16:59:12 +0100

    Add a logger for LED binary regions

---
+++ workflows/social/Extensions/FormatBinaryRegions.cs
@@ -0,0 +1,60 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Bonsai.Vision;
+using Bonsai.Harp;
+
+[Combinator]
+[Description("Converts timestamped binary regions into a sequence of Harp messages.")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class FormatBinaryRegions
+{
+    private int address = 200;
+    public int Address
+    {
+        get { return address; }
+        set { address = value; }
+    }
+    
+
+    public IObservable<HarpMessage> Process(IObservable<Timestamped<ConnectedComponent>> source)
+    {
+        return source.Select(x =>
+        {
+            var region = x.Value;
+            var timestamp = x.Seconds;
+            return HarpMessage.FromSingle(
+                address,
+                timestamp,
+                MessageType.Event,
+                region.Centroid.X,
+                region.Centroid.Y,
+                (float)region.Orientation,
+                (float)region.MajorAxisLength,
+                (float)region.MinorAxisLength,
+                (float)region.Area);
+        });
+    }
+
+    public IObservable<HarpMessage> Process(IObservable<Timestamped<ConnectedComponentCollection>> source)
+    {
+        return source.SelectMany(x =>
+        {
+            var regions = x.Value;
+            var timestamp = x.Seconds;
+            return regions.Select((region, index) => HarpMessage.FromSingle(
+                address,
+                timestamp,
+                MessageType.Event,
+                region.Centroid.X,
+                region.Centroid.Y,
+                (float)region.Orientation,
+                (float)region.MajorAxisLength,
+                (float)region.MinorAxisLength,
+                (float)region.Area,
+                index));
+        });
+    }
+}
