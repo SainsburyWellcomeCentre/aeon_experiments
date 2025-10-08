aad22113edbae4aab402650641d8280cda8940c6
NeuroThom <t.ryan@neurogears.org>
2025-10-09 00:44:59 +0100

    Avoid local ONIX package build

--- workflows/ephys/Ephys-AEONX1.cmd
+++ workflows/ephys/Ephys-AEONX1.cmd
@@ -1,4 +1,4 @@
 :loop
-powershell -ep Bypass -c "& ..\..\bonsai\Bonsai.exe --lib $(Resolve-Path ..\onix-refactor\OpenEphys.Onix\OpenEphys.Onix\bin\x64\Release\net472\) Ephys-AEONX1.bonsai %* *>&1 | tee -a Ephys-AEONX1.log"
+powershell -ep Bypass -c "& ..\..\bonsai\Bonsai.exe Ephys-AEONX1.bonsai %* *>&1 | tee -a Ephys-AEONX1.log"
 timeout /t 5
 goto :loop
