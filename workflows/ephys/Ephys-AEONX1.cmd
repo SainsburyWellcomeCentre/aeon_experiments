5354dcf24edac13cb7c72291d0c856138ee48a66
NeuroThom <t.ryan@neurogears.org>
2026-06-04 15:55:59 +0100

    Add workflow to send alert on auto-restart

--- workflows/ephys/Ephys-AEONX1.cmd
+++ workflows/ephys/Ephys-AEONX1.cmd
@@ -1,4 +1,5 @@
 :loop
-powershell -ep Bypass -c "& '..\..\bonsai\Bonsai.exe' --no-editor Ephys-AEONX1.bonsai *>&1 | tee -a Ephys-AEONX1.log"
+powershell -ep Bypass -c "& '..\..\.bonsai\Bonsai.exe' --no-editor Ephys-AEONX1.bonsai *>&1 | tee -a Ephys-AEONX1.log"
+powershell -ep Bypass -c "& '..\..\.bonsai\Bonsai.exe' --no-editor RestartAlert.bonsai *>&1 | tee -a Ephys-AEONX1.log"
 timeout /t 20
 goto :loop
